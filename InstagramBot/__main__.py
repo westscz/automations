0f554d6bd88d9db26c162b79b3202e0729533caf
westscz <westscz@gmail.com>
2018-02-05 22:13:48 +0100

    InstagramBot Added logger

--- InstagramBot/__main__.py
+++ InstagramBot/__main__.py
@@ -5,6 +5,16 @@ Simply implementation of instagram bot to like media from tags
 from InstagramAPI import InstagramAPI
 import time
 import fire
+import logging
+import pprint
+
+logger = logging.getLogger('spam_application')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
+
+pp = pprint.PrettyPrinter(indent=4)
 
 
 def login(username, password):
@@ -21,29 +31,43 @@ def like_new_from_tags(ap, tags_list, likes_number):
 
 
 def like_new_from_tag(ap, tag):
-    print(tag)
+    logger.info("Like new from tag: {}".format(tag))
     ap.tagFeed(tag)
     i = ap.LastJson['items'][0]
-    print(i)
+    # pp.pprint(i)
     ap.like(i['id'])
 
     time.sleep(80)
 
 
 def comment_new_from_tag(ap, tag):
-    print(tag)
+    logger.info("Comment new from tag: {}".format(tag))
     d = {"lomography":["Nice picture!"]}
 
     ap.tagFeed(tag)
     i = ap.LastJson['items'][0]
-    print(i)
-    ap.comment(i['id'], "Nice picture!")
+
+    # pp.pprint(i)
+    ap.comment(i['id'], "I love this analog vibe :D")
     time.sleep(80)
+
+
+def follow_new_from_tag_author(ap, tag):
+    logger.info("Follow new from tag: {}".format(tag))
+    ap.tagFeed(tag)
+    i = ap.LastJson['items'][0]
+    # pp.pprint(i)
+    user = i.get("user").get("pk")
+    print("www.instagram.com/{}".format(i.get("user").get("username")))
+    ap.follow(user)
+    time.sleep(100)
+    ap.unfollow(user)
 
 def fire_cli(logins, password):
     ap = login(logins, password)
-    comment_new_from_tag(ap, "lomography")
-    like_new_from_tags(ap, ["lomography", "35mm", 'filmisnotdead'], 100)
+    # comment_new_from_tag(ap, "lomography")
+    # follow_new_from_tag_author(ap, "lomography")
+    like_new_from_tags(ap, ["lomography", "35mm", 'filmisnotdead'], 156)
 
 
 if __name__ == '__main__':
