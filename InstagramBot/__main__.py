ebb7ed569abb3b2d5120da0b762f9cf8c9304682
westscz <westscz@gmail.com>
2018-02-06 20:06:15 +0100

    InstagramBot Added config file, requirements updated, new functions to search and show media item

--- InstagramBot/__main__.py
+++ InstagramBot/__main__.py
@@ -7,6 +7,7 @@ import time
 import fire
 import logging
 import pprint
+import yaml
 
 logger = logging.getLogger('spam_application')
 logger.setLevel(logging.DEBUG)
@@ -15,6 +16,16 @@ ch.setLevel(logging.DEBUG)
 logger.addHandler(ch)
 
 pp = pprint.PrettyPrinter(indent=4)
+
+
+
+
+
+class YamlConfig():
+    def __init__(self):
+        with open("config.yml", 'r') as ymlfile:
+            self.cfg = yaml.load(ymlfile)
+        pp.pprint(self.cfg)
 
 
 def login(username, password):
@@ -32,9 +43,8 @@ def like_new_from_tags(ap, tags_list, likes_number):
 
 def like_new_from_tag(ap, tag):
     logger.info("Like new from tag: {}".format(tag))
-    ap.tagFeed(tag)
-    i = ap.LastJson['items'][0]
-    # pp.pprint(i)
+    i = get_media_from_tag(ap, tag, newest=False)
+    show_info_for_media_item(i)
     ap.like(i['id'])
 
     time.sleep(80)
@@ -44,31 +54,53 @@ def comment_new_from_tag(ap, tag):
     logger.info("Comment new from tag: {}".format(tag))
     d = {"lomography":["Nice picture!"]}
 
-    ap.tagFeed(tag)
-    i = ap.LastJson['items'][0]
-
-    # pp.pprint(i)
+    i = get_media_from_tag(ap, tag, last_count=-1, newest=False)
+    show_info_for_media_item(i)
     ap.comment(i['id'], "I love this analog vibe :D")
     time.sleep(80)
 
 
 def follow_new_from_tag_author(ap, tag):
     logger.info("Follow new from tag: {}".format(tag))
-    ap.tagFeed(tag)
-    i = ap.LastJson['items'][0]
-    # pp.pprint(i)
+    i = get_media_from_tag(ap, tag, newest=False)
+    show_info_for_media_item(i)
     user = i.get("user").get("pk")
     print("www.instagram.com/{}".format(i.get("user").get("username")))
     ap.follow(user)
     time.sleep(100)
     ap.unfollow(user)
 
-def fire_cli(logins, password):
-    ap = login(logins, password)
-    # comment_new_from_tag(ap, "lomography")
-    # follow_new_from_tag_author(ap, "lomography")
-    like_new_from_tags(ap, ["lomography", "35mm", 'filmisnotdead'], 156)
 
+def get_media_from_tag(ap, tag, last_count=20, newest=True):
+    ap.tagFeed(tag)
+    if newest:
+        return ap.LastJson['items'][0]
+    else:
+        like_count = 0
+        item = None
+        for x in ap.LastJson['items'][:last_count]:
+            if x['like_count'] > like_count and not x['has_liked']:
+                item = x
+                like_count = x['like_count']
+        return item
+
+
+def show_info_for_media_item(item):
+    full_name = item.get("user").get("full_name")
+    media_id = item.get('code')
+    url = "https://instagram.com/p/{}".format(media_id)
+    logger.info("{fn}: {url}".format(fn=full_name, url=url))
+
+
+def fire_cli(logins, password):
+    config = YamlConfig()
+
+
+    ap = login(logins, password)
+    comment_new_from_tag(ap, "lomography")
+    follow_new_from_tag_author(ap, "lomography")
+    like_new_from_tags(ap, config.cfg['tags'], 156)
+    # get_media_from_tag(ap, "lomography")
 
 if __name__ == '__main__':
     fire.Fire(fire_cli)
