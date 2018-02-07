2f9f2c03daea73a561db4df102b7bea74cea8959
westscz <westscz@gmail.com>
2018-02-07 19:43:44 +0100

    InstagramBot Script moved to class!

--- InstagramBot/__main__.py
+++ InstagramBot/__main__.py
@@ -29,78 +29,75 @@ class YamlConfig:
         pp.pprint(self.cfg)
 
 
-def login(username, password):
-    api = InstagramAPI(username, password)  # Instantiate the class
-    api.login()  # Send a login request
-    return api
+class InstagramBot(object):
+    def __init__(self):
+        self.IAPI = None
+
+    def login(self, username, password):
+        self.IAPI = InstagramAPI(username, password)  # Instantiate the class
+        self.IAPI.login()  # Send a login request
+
+    def like_new_from_tags(self, tags_list, likes_number):
+        number = int(likes_number / len(tags_list))
+        for i in range(0, likes_number):
+            for tag in tags_list:
+                self.like_new_from_tag(tag)
+
+    def like_new_from_tag(self, tag):
+        logger.info("Like new from tag: {}".format(tag))
+        i = self.get_media_from_tag(tag, newest=False)
+        self.show_info_for_media_item(i)
+        self.IAPI.like(i['id'])
+
+        time.sleep(80)
+
+    def comment_new_from_tag(self, tag):
+        logger.info("Comment new from tag: {}".format(tag))
+        d = {"lomography": ["Nice picture!"]}
+
+        i = self.get_media_from_tag(tag, last_count=-1, newest=False)
+        self.show_info_for_media_item(i)
+        self.IAPI.comment(i['id'], "I love this analog vibe :D")
+        time.sleep(80)
+
+    def follow_new_from_tag_author(self, tag):
+        logger.info("Follow new from tag: {}".format(tag))
+        i = self.get_media_from_tag(tag, newest=False)
+        self.show_info_for_media_item(i)
+        user = i.get("user").get("pk")
+        print("www.instagram.com/{}".format(i.get("user").get("username")))
+        self.IAPI.follow(user)
+        time.sleep(100)
+        self.IAPI.unfollow(user)
+
+    def get_media_from_tag(self, tag, last_count=20, newest=True):
+        self.IAPI.tagFeed(tag)
+        if newest:
+            return self.IAPI.LastJson['items'][0]
+        else:
+            like_count = 0
+            item = None
+            for x in self.IAPI.LastJson['items'][:last_count]:
+                if x['like_count'] > like_count and not x['has_liked']:
+                    item = x
+                    like_count = x['like_count']
+            return item
+
+    def show_info_for_media_item(self, item):
+        full_name = item.get("user").get("full_name")
+        media_id = item.get('code')
+        url = "https://instagram.com/p/{}".format(media_id)
+        logger.info("{fn}: {url}".format(fn=full_name, url=url))
 
 
-def like_new_from_tags(ap, tags_list, likes_number):
-    number = int(likes_number / len(tags_list))
-    for i in range(0, likes_number):
-        for tag in tags_list:
-            like_new_from_tag(ap, tag)
-
-
-def like_new_from_tag(ap, tag):
-    logger.info("Like new from tag: {}".format(tag))
-    i = get_media_from_tag(ap, tag, newest=False)
-    show_info_for_media_item(i)
-    ap.like(i['id'])
-
-    time.sleep(80)
-
-
-def comment_new_from_tag(ap, tag):
-    logger.info("Comment new from tag: {}".format(tag))
-    d = {"lomography":["Nice picture!"]}
-
-    i = get_media_from_tag(ap, tag, last_count=-1, newest=False)
-    show_info_for_media_item(i)
-    ap.comment(i['id'], "I love this analog vibe :D")
-    time.sleep(80)
-
-
-def follow_new_from_tag_author(ap, tag):
-    logger.info("Follow new from tag: {}".format(tag))
-    i = get_media_from_tag(ap, tag, newest=False)
-    show_info_for_media_item(i)
-    user = i.get("user").get("pk")
-    print("www.instagram.com/{}".format(i.get("user").get("username")))
-    ap.follow(user)
-    time.sleep(100)
-    ap.unfollow(user)
-
-
-def get_media_from_tag(ap, tag, last_count=20, newest=True):
-    ap.tagFeed(tag)
-    if newest:
-        return ap.LastJson['items'][0]
-    else:
-        like_count = 0
-        item = None
-        for x in ap.LastJson['items'][:last_count]:
-            if x['like_count'] > like_count and not x['has_liked']:
-                item = x
-                like_count = x['like_count']
-        return item
-
-
-def show_info_for_media_item(item):
-    full_name = item.get("user").get("full_name")
-    media_id = item.get('code')
-    url = "https://instagram.com/p/{}".format(media_id)
-    logger.info("{fn}: {url}".format(fn=full_name, url=url))
-
-
-def fire_cli(logins, password):
+def fire_cli(user_login, user_password):
     config = YamlConfig()
 
-
-    ap = login(logins, password)
-    comment_new_from_tag(ap, "lomography")
-    follow_new_from_tag_author(ap, "lomography")
-    like_new_from_tags(ap, config.tags, 156)
+    ibot = InstagramBot()
+    ibot.login(user_login, user_password)
+    ibot.comment_new_from_tag("lomography")
+    ibot.follow_new_from_tag_author("lomography")
+    ibot.like_new_from_tags(config.tags, 156)
     # get_media_from_tag(ap, "lomography")
 
 
