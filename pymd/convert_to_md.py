3621d48ed30f22d15b179598d3a3f40dbfc12d09
Jaroslaw Piszczala <(none)>
2020-11-11 15:51:15 +0100

    push pymd

---
+++ pymd/convert_to_md.py
@@ -0,0 +1,25 @@
+is_article = False
+is_code = True
+
+md = open("test.md", "w")
+
+def write(open_file, line):
+    print(line, end="")
+    open_file.write(line)
+
+with open("mdpy.py", "r") as f:
+    for i, l in enumerate(f.readlines()):
+        if l[:3] == '"""':
+            is_article =  not(is_article)
+            is_code = not(is_article)
+            if is_code:
+                write(md, "```python\n")
+            elif i:
+                write(md, "```\n")
+        else:
+            if (is_code and not l=="\n") or is_article:
+                write(md, f"{l}\n")
+if not is_article:
+    write(md, "```\n")
+md.close()
+
---
+++ pymd/mdpy.py
@@ -0,0 +1,22 @@
+"""
+# Lists!
+
+In python you can make trully awesome things!
+For example, we can create empty list
+"""
+l = []
+"""
+After that we can add new things to list.
+If list is empty, python threats it as False value
+<!--s-->
+"""
+assert not l
+"""
+Lets add some things to list!
+"""
+l.append(1)
+l.append(2)
+assert len(l) == 2
+"""
+And thats all for now, and we saw how awesome thing we can do
+"""
