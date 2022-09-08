25709674bc49493c4df9463d8804f0f3449216b6
Jarosław Piszczała <westscz@gmail.com>
2022-09-08 10:31:10 +0200

    Moved pocket_escape to new repository

--- pocket_escape/code/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-def configure_logger(name):
-    log = logging.getLogger(name)
-    log.setLevel(logging.INFO)
-    stream = logging.StreamHandler()
-    log.addHandler(stream)
-    return log
